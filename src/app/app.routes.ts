cd24c018881a11686f3f5a4b1a6f10cac51c10a1
Bahdan Kalishok <bahdan_kalishok@epam.com>
2025-07-29 17:55:51 +0500

    Update routes with login and guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { authGuard } from '../shared/guards/index'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: Home, canActivate: [authGuard] },
+    { path: 'login', loadComponent: () => import('../app/login/login').then(c => c.Login) },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '**', redirectTo: '/home' }
+];
